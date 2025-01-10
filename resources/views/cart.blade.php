7f7a447e83301e3753fc2689deccf107e1c892a8
Krishna Mandal <mandalkrishna739@gmail.com>
2025-01-10 22:01:46 +0545

    Update Cart  Quantity

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -68,8 +68,17 @@
                         <td>
                         <div class="qty-control position-relative">
                             <input type="number" name="quantity" value="{{$item->qty}}" min="1" class="qty-control__number text-center">
-                            <div class="qty-control__reduce">-</div>
-                            <div class="qty-control__increase">+</div>
+                            <form action="{{ route('cart.qty.decrease', ['rowId'=>$item->rowId]) }}" method="post">
+                                @csrf
+                                @method('PUT')
+                                <div class="qty-control__reduce">-</div>
+                            </form>
+
+                            <form action="{{ route('cart.qty.increase', ['rowId'=>$item->rowId]) }}" method="post">
+                                @csrf
+                                @method('PUT')
+                                <div class="qty-control__increase">+</div>
+                            </form>
                         </div>
                         </td>
                         <td>
@@ -143,3 +152,16 @@
   </main>
 
 @endsection
+
+@push('scripts')
+    <script>
+        $(function(){
+            $(".qty-control__increase").on('click', function(){
+                $(this).closest('form').submit();
+            });
+            $(".qty-control__reduce").on('click', function(){
+                $(this).closest('form').submit();
+            });
+        })
+    </script>
+@endpush
